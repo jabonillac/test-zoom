45712021e69ca2dfd65cf495da35e7cd47f57c7e
Alejandro Bonilla <jabonillac@gmail.com>
2021-03-04 21:59:15 -0500

    code commit

---
+++ docroot/config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', 'OjGcLg1cQZiKbwNaZSQ7aA');
+define('CLIENT_SECRET', '4W2WU2kj7jOF7iIPmcqYL4GP6TnFUhGm');
+define('REDIRECT_URI', 'http://73a6b7297b9f.ngrok.io/zoom/callback.php');
